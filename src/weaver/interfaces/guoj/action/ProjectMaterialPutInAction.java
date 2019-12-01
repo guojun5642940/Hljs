533561d09110ecfed1e8708f0f0807ea866faa82
guojun <1021015989@qq.com>
2019-12-01 21:01:54 +0800

    first-commit

--- src/weaver/interfaces/guoj/action/ProjectMaterialPutInAction.java
+++ src/weaver/interfaces/guoj/action/ProjectMaterialPutInAction.java
@@ -9,6 +9,7 @@ import weaver.workflow.request.RequestComInfo;
 import weaver.workflow.workflow.WorkflowComInfo;
 import weaver.interfaces.guoj.util.ActionMapUtil;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,40 +45,60 @@ public class ProjectMaterialPutInAction  implements Action {
         String rkdbh = Util.null2String(dataMap.get("rkdbh".toLowerCase()).toString());
         String rq = Util.null2String(dataMap.get("rq".toLowerCase()).toString());
 
-        //查询到建模表中对应的项目数据
-        String sql = "select id,zrksl,sjkcsl,rkzj from uf_yskm where xmbh = '"+xmbh+"'";
+        String sql = "";
+        String insertSql = "";
+        String updateSql = "";
         RecordSet rs = new RecordSet();
-        RecordSet rsUpdate = new RecordSet();
-        rs.execute(sql);
-        if(rs.next()){
-            String id = Util.null2String(rs.getString("id"));
-            int zrksl = Util.getIntValue(rs.getString("zrksl"));//总入库数量
-            int sjkcsl = Util.getIntValue(rs.getString("sjkcsl"));//实际库存数量
-            double rkzj = Util.getDoubleValue(rs.getString("rkzj"));//入库总价
+        RecordSet rsInsert = new RecordSet();
+        ArrayList<HashMap> mx1List = new ArrayList<>();
+        mx1List = aUtil.getMXDataMap(request,1);
+        for(Map map:mx1List){
+            String yskm = map.get("yskm").toString();
+            String clkm = map.get("clkm").toString();
+            String sssl = map.get("sssl").toString();
+            String dj = map.get("dj").toString();
+            String je = map.get("je").toString();
+            String bz = map.get("bz").toString();
 
-            bs.writeLog("requestid=["+requestId+"],xmmc=["+xmmc+"]查询建模数据ID["+id+"]");
-            ArrayList<HashMap> mx1List = new ArrayList<>();
-            for(Map map:mx1List){
-                String clkm = map.get("clkm").toString();
-                String sssl = map.get("sssl").toString();
-                String dj = map.get("dj").toString();
-                String je = map.get("je").toString();
-                String bz = map.get("bz").toString();
+            //查询到建模表中对应的项目数据
+            sql = "select id,zcksl from uf_yskm where xmbh = '"+xmbh+"' and km = '"+yskm+"'";
+            bs.writeLog("requestid=["+requestId+"],xmmc=["+xmmc+"]查询建模主表sql["+sql+"]");
+            rs.execute(sql);
 
-                sql = "insert into uf_yskm_dt1 (mainid,rkdh,rkrq,clkm,rksl,rkdj,rkje,bz,lcid)" +
+            if(rs.next()){
+                String id = Util.null2String(rs.getString("id"));
+                double zcksl = Util.getDoubleValue(rs.getString("zcksl"),0);//总出库数量
+                insertSql = "insert into uf_yskm_dt1 (mainid,rkdh,rkrq,clkm,rksl,rkdj,rkje,bz,lcid)" +
                         " values ('"+id+"','"+rkdbh+"','"+rq+"','"+clkm+"','"+sssl+"','"+dj+"','"+je+"','"+bz+"','"+requestId+"')";
-                bs.writeLog("requestid=["+requestId+"],xmmc=["+xmmc+"]插入建模数据sql["+sql+"]");
-                rsUpdate.execute(sql);
-                zrksl += Integer.parseInt(sssl);
-                sjkcsl += Integer.parseInt(sssl);
-                rkzj += Double.parseDouble(je);
+                bs.writeLog("requestid=["+requestId+"],xmmc=["+xmmc+"]插入建模数据sql["+insertSql+"]");
+                rsInsert.execute(insertSql);
+                updateModeinfo(id,zcksl,requestId);
+            }else{
+                bs.writeLog("requestid=["+requestId+"],xmmc=["+xmmc+"]未查到"+xmbh+"----"+yskm+"对应的建模数据");
             }
-            //更新项目建模数据主表
-            sql = "update uf_yskm set zrksl = '"+zrksl+"',sjkcsl = '"+sjkcsl+"',rkzj = '"+rkzj+"' where id = '"+id+"'";
-            bs.writeLog("requestid=["+requestId+"],xmmc=["+xmmc+"]更新建模主表数据sql["+sql+"]");
-        }else{
-            bs.writeLog("requestid=["+requestId+"],xmmc=["+xmmc+"]未查到对应的建模数据");
         }
         return SUCCESS;
+    }
+
+    /**
+     * 更新建模主表数据
+     */
+    public void updateModeinfo(String id,double zcksl,String requestId){
+        double zrksl = 0;//总入库数量
+        double rkzj = 0;//入库总价
+        String sql = "select * from uf_yskm_dt1 where mainid = '"+id+"'";
+        RecordSet rs = new RecordSet();
+        rs.execute(sql);
+        while(rs.next()){
+            double rksl = Util.getDoubleValue(rs.getString("rksl"));
+            double rkje = Util.getDoubleValue(rs.getString("rkje"));
+            zrksl += rksl;
+            rkzj += rkje;
+        }
+        double kcpjdj = rkzj/zrksl;
+        DecimalFormat df = new DecimalFormat("#.0000");
+        sql = "update uf_yskm set zrksl = '"+zrksl+"',rkzj='"+rkzj+"',sjkcsl='"+(zrksl-zcksl)+"',kcpjdj='"+df.format(kcpjdj)+"' where id = '"+id+"'";
+        bs.writeLog("requestid=["+requestId+"],updateModeinfo--"+sql);
+        rs.execute(sql);
     }
 }
